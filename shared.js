2d096870965a53fef943c0ddb2898e27d0cccaad
Khushi <khushisrivastava53@gmail.com>
2020-06-02 00:08:42 +0530

    Added a side nav bar(hambar)

--- shared.js
+++ shared.js
@@ -1,7 +1,9 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
-var noButton = document.querySelector('.modal__action--negative')
+var noButton = document.querySelector('.modal__action--negative');
 var selectPlanButton = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav')
 
 for(var i = 0; i < selectPlanButton.length; i++){
     selectPlanButton[i].addEventListener('click', function() { 
@@ -10,10 +12,18 @@ for(var i = 0; i < selectPlanButton.length; i++){
     });
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function() {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
 noButton.addEventListener('click', closeModal);
 
 function closeModal(){
     modal.style.display = 'none';
     backdrop.style.display = 'none';
 }
+
+toggleButton.addEventListener('click', function(){
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
