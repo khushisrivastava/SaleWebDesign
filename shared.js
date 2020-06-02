9e72e423620f0f73b3a3cdb40d52dc49e248c008
Khushi <khushisrivastava53@gmail.com>
2020-06-02 22:17:37 +0530

    Added side nav on all the pages and adding and removing classes in js file

--- shared.js
+++ shared.js
@@ -7,23 +7,35 @@ var mobileNav = document.querySelector('.mobile-nav')
 
 for(var i = 0; i < selectPlanButton.length; i++){
     selectPlanButton[i].addEventListener('click', function() { 
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        modal.classList.add('open')
+        backdrop.classList.add('open')
     });
 }
 
 backdrop.addEventListener('click', function() {
-    mobileNav.style.display = 'none';
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open')
     closeModal();
 });
-noButton.addEventListener('click', closeModal);
+
+if (noButton){
+    noButton.addEventListener('click', closeModal);
+}
 
 function closeModal(){
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    if (modal){
+        modal.classList.remove('open')
+    }
+    backdrop.classList.remove('open')
 }
 
 toggleButton.addEventListener('click', function(){
-    mobileNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open')
+    backdrop.classList.add('open')
 });
